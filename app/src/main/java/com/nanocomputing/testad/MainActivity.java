051b16dbb3829e81e94e709bf12d4e1a9613d921
nixswinner <nixondeveloper@gmail.com>
2019-04-01 11:23:41 +0300

    can refresh

--- app/src/main/java/com/nanocomputing/testad/MainActivity.java
+++ app/src/main/java/com/nanocomputing/testad/MainActivity.java
@@ -2,6 +2,7 @@ package com.nanocomputing.testad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 import com.ads.mobitechadslib.AdsModel;
@@ -9,6 +10,8 @@ import com.ads.mobitechadslib.MobiAdBanner;
 import com.ads.mobitechadslib.MobitechAds;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import io.reactivex.Observable;
 import io.reactivex.Observer;
@@ -40,37 +43,44 @@ public class MainActivity extends AppCompatActivity {
 
 
         // ----------------------Banner Ad --------------------.
-       mobiAdBanner = findViewById(R.id.bannerAd);
+        mobiAdBanner = findViewById(R.id.bannerAd);
         mobiAdBanner.setOnClickListener(v -> {
             mobiAdBanner.viewBannerAd(MainActivity.this,
                     adsModel.getAd_urlandroid());
         });
-        //change the category id here
-       Observable<Response> observable = getBannerAd(adCategory);
-       observable.subscribe(new Observer<Response>() {
-           @Override
-           public void onSubscribe(Disposable d) {
-               disposable.add(d);
-           }
-           @Override
-           public void onNext(Response response) {
-               try {
-                   adsModel = getBannerAdValues(response.body().string());
-                   mobiAdBanner.showAd(MainActivity.this,
-                           adsModel.getAd_upload());
-               } catch (IOException e) {
-                   e.printStackTrace();
-               }
-          }
-           @Override
-           public void onError(Throwable e) {
-           }
-           @Override
-           public void onComplete() {
-           }
-       });
+        Timer timer = new Timer();
+        TimerTask myTask = new TimerTask() {
+            @Override
+            public void run() {
+                Observable<Response> observable = getBannerAd(adCategory);
+                observable.subscribe(new Observer<Response>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposable.add(d);
+                    }
+                    @Override
+                    public void onNext(Response response) {
+                        try {
+                            adsModel = getBannerAdValues(response.body().string());
+                            mobiAdBanner.showAd(getApplicationContext(),
+                                    adsModel.getAd_upload());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e("Banner", "onError : "+e.getMessage() );
+                    }
+                    @Override
+                    public void onComplete() {
+                    }
+                });
+            }
+        };timer.schedule(myTask, 100, 20000);//refresh after
 
-       //...............................end of banner ad ........................
+//...............................end of banner ad ........................
+
 
         Button showBanner = findViewById(R.id.showbanner);
         showBanner.setOnClickListener(v->{
